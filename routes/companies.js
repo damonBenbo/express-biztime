570245b57ddfb30161327edbdb9b436a60c2a354
damonBenbo <damon.benbo@gmail.com>
2023-07-30 11:22:56 -0600

    added routes for / and /:code for companies.js

---
+++ routes/companies.js
@@ -0,0 +1,62 @@
+// Routes for companies
+
+const express = require("express");
+const slugify = require("slugify");
+const ExpressError = require("../expressError");
+const db = require("../db");
+
+let router = new express.Router();
+
+// GET / => list of companies
+
+// => {companies: [{code, name, descrip}, {code, name, descripo}, ...]}
+
+router.get("/", async (req, res, next) => {
+    try {
+        const result = await db.query(
+            `SELECT code, name
+            FROM companies
+            ORDER BY name`
+        );
+        return res.json({"companies": result.rows});
+    }
+    catch(err) {
+        return next(err);
+    }
+});
+
+// GET /[code] => details on company
+// => {company: {code, name, descripo, invoices: [id, ...]}}
+
+router.get("/:code", async (req, res, next) => {
+    try {
+        let code = req.params.code;
+
+        const compResult = await db.query(
+            `SELECT code, name, description
+            FROM companies
+            WHERE code = $1`, [code]
+        );
+
+        const invResult = await db.query(
+            `SELECT id
+            FROM invoices
+            WHERE comp_code = $1`, [code]
+        );
+
+        if (compResult.rows.length === 0) {
+            throw new ExpressError(`No such company: ${code}`, 404)
+        }
+
+        const company = compResult.rows[0];
+        const invoices = invResult.rows;
+
+        company.invoices = invoices.map(inv => inv.id);
+
+        return res.json({"company": company});
+    }
+
+    catch(err) {
+        return next(err);
+    }
+});
